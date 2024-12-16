636a34418600a93a39486bf9dc5ab1ea50068f3f
F1tz111 <mustafashakeel111@gmail.com>
2024-12-17 01:01:00 +0500

    made some changes, css/js is working now and layout is created

---
+++ app/javascript/application.js
@@ -0,0 +1,13 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+import './animations.min'
+import './bootstrap'
+import './appear.min'
+import './jquery-1.11.2'
+import './jquery-ui'
+import './jquery.fancybox.min'
+import './pluginsf700'
+import './velocity.min'
+import './velocity.ui.min'
